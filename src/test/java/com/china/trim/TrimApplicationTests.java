91b1d1c139f96aa8ed17cad4f25f12d8490aa015
runoob <test@runoob.com>
2020-08-20 17:35:50 +0800

    项目创建

---
+++ src/test/java/com/china/trim/TrimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.china.trim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
